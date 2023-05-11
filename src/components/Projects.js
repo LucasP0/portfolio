f3262947732ead8cb377f3dcf2c680043864bfdb
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2023-05-11 14:58:29 -0300

    atualização do portifolio

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -53,8 +53,8 @@ export const Projects = () => {
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h2>Protos</h2>
+                <p>Esta área é dedicada a projetos atuais e futuros desenvolvidos por mim </p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
