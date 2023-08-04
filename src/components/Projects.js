a43d105295b16a135cc0cc202981f994f066e418
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2023-08-04 14:13:55 -0300

    Adicionando novo projeto

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,52 +5,10 @@ import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
 // IMPORTAÇÃO DE IMGS
-import projImg1 from "../assets/img-project/img 21.png";
-import breve from "../assets/img-project/breve.png";
-import Responsive from "../assets/img-project/Responsive-side-bar.png"
-import Agenda_Ts from "../assets/img-project/Agenda-Ts.png";
-import Next_Tailwind from "../assets/img-project/Estilização com Tailwind.png";
+
+import { projects } from "./FetchProjects";
 
 export const Projects = () => {
-
-  const projects = [
-    {
-      title: "Login",
-      description: "Pagina de Login com React",
-      imgUrl: projImg1,
-      link: "https://incandescent-vacherin-5c77a3.netlify.app/",
-    },
-    {
-      title: "Sidebar Menu",
-      description: "Responsive Sidebar Menu com JS + dark theme",
-      imgUrl: Responsive,
-      link: "https://lucasp0.github.io/Responsive-Siderbar-Menu/",
-    },
-    {
-      title: "Agenda de Tarefas",
-      description: "Agenda com Ts + React",
-      imgUrl: Agenda_Ts,
-      link: "https://delightful-creponne-300ef8.netlify.app/",
-    },
-    {
-      title: "Estilização Com Tailwind",
-      description: "Um projeto apenas com estilização no next utilizando Tailwind",
-      imgUrl: Next_Tailwind,
-      link: "https://main--venerable-chimera-3d2007.netlify.app/"
-    },
-    {
-      title: "Em Breve",
-      description: "Novos sites virão",
-      imgUrl: breve,
-    },
-    {
-      title: "Em Breve",
-      description: "Novos sites virão",
-      imgUrl: breve,
-    },
-    
-  ];
-
   return (
     <section className="project" id="project">
       <Container>
