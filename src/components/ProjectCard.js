5e5bcfd1f5f28857b674cfc1641a9aa79a1391d6
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2023-10-13 15:08:19 -0300

    atualização

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,7 +3,7 @@ import { Col } from "react-bootstrap";
 export const ProjectCard = ({ title, description, imgUrl, link }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <a  id='link-P' target={"_blank"} href={link} rel="noreferrer">
+      <a id='link-P' target={"_blank"} href={link} rel="noreferrer">
         <div className="proj-imgbx">
           <img src={imgUrl} />
           <div className="proj-txtx">
